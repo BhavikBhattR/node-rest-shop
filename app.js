bc51e6432270d9eea519d096bbb6601216ae7c60
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-04-01 15:09:03 +0530

    body parsing and CORS handling done

--- app.js
+++ app.js
@@ -4,13 +4,32 @@ const app = express()
 
 const morgan = require('morgan');
 
+const bodyParser = require('body-parser');
+
+
 const productsRouter = require('./api/routes/products')
 const ordersRouter = require('./api/routes/orders')
 
 app.use(morgan('dev'))
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Origin, X-Requested-With"
+        )
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods")
+        return res;
+    }
+    next()
+})
+
 app.use('/products', productsRouter)
 app.use('/orders', ordersRouter)
+
 
 app.use((req,res,next)=>{
     const err = new Error('tu jo dhundh rha hai wo nahi he');
