fdfd45a8d4b536fa4629935bbbb018038a8cd04b
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-04-18 23:30:34 +0530

    logging CORS error-handling and mongodb connection done

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,36 +2,66 @@ const express = require('express');
 
 const router = express.Router()
 
+const Product = require('../models/product');
+
+const mongoose = require('mongoose');
 
 router.get('/', (req,res,next)=>{
-    res.status(200).json({
-        message: "handling get req of /products 🤟"
+   Product.find()
+   .exec()
+   .then(docs =>{
+        console.log(docs);
+        res.status(200).json(docs)
+   })
+   .catch(err =>{
+    console.log(err);
+    res.status(500).json({
+        error: err
     })
+   });
 })
 
 router.post('/', (req,res,next)=>{
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
-    res.status(201).json({
-        message: "handling post req of /products ✨",
-        createProduct: product
+    });
+    product.save().then(result =>{
+        console.log(result);
+        res.status(201).json({
+            message: "handling post req of /products ✨",
+            createProduct: product
+        })
     })
+    .catch(err => {
+        err => console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
 })
 
 router.get('/:productID', (req,res,next)=>{
     const id = req.params.productID;
-    if(id === 'special'){
-        res.status(200).json({
-            message: 'you discovered the special id',
-            id: id
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log('from db',doc)
+        if(doc){
+            res.status(200).json(doc)
+        }else{
+            return res.status(404).json({
+                message: "no valid entry for given id"
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+                err
         })
-    }else{
-        res.status(200).json({
-            message: 'you passed an ID'
-        })
-    }
+    });
 })
 
 router.patch('/:productID', (req,res,next)=>{
@@ -41,9 +71,15 @@ router.patch('/:productID', (req,res,next)=>{
 })
 
 router.delete('/:productID', (req,res,next)=>{
-    res.status(200).json({
-        message: 'deleted the product'
+    const id = req.params.productID;
+    Product.findOneAndRemove({
+        _id : id
     })
+    .exec()
+    .then(result =>{
+        res.status(200).json(result)
+    })
+    .catch()
 })
 
 module.exports = router
