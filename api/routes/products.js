b8010c2663a77f855a5af7e6923a2c72d2042cda
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-04-21 04:59:12 +0530

    modified response for /products

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,13 +5,34 @@ const router = express.Router()
 const Product = require('../models/product');
 
 const mongoose = require('mongoose');
+const e = require('express');
 
 router.get('/', (req,res,next)=>{
    Product.find()
+   .select('name price _id')
    .exec()
    .then(docs =>{
-        console.log(docs);
-        res.status(200).json(docs)
+        const response = {
+            count: docs.length,
+            products: docs.map(doc => {
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: `http://localhost:3000/products/${doc._id}`
+                    }
+                }
+            })
+        }
+        if(docs.length >=0){
+            res.status(200).json(response)
+        }else{
+            res.status(404).json({
+                message: "no entries found"
+            })
+        }
    })
    .catch(err =>{
     console.log(err);
@@ -30,8 +51,16 @@ router.post('/', (req,res,next)=>{
     product.save().then(result =>{
         console.log(result);
         res.status(201).json({
-            message: "handling post req of /products ✨",
-            createProduct: product
+            message: "Created Product Successfully ✨",
+            createProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: `http://localhost:3000/products/${result._id}`
+                }
+            }
         })
     })
     .catch(err => {
@@ -45,11 +74,19 @@ router.post('/', (req,res,next)=>{
 router.get('/:productID', (req,res,next)=>{
     const id = req.params.productID;
     Product.findById(id)
+    .select('name price _id')
     .exec()
     .then(doc => {
         console.log('from db',doc)
         if(doc){
-            res.status(200).json(doc)
+            res.status(200).json({
+                product: doc,
+                request: {
+                    type: 'GET',
+                    description: 'Get all Products',
+                    url: 'http://localhost:3000/products'
+                }
+            })
         }else{
             return res.status(404).json({
                 message: "no valid entry for given id"
@@ -65,8 +102,32 @@ router.get('/:productID', (req,res,next)=>{
 })
 
 router.patch('/:productID', (req,res,next)=>{
-    res.status(200).json({
-        message: 'updated the product'
+    const id = req.params.productID;
+    const updatedOps = {};
+    for(const ops of req.body){
+        updatedOps[ops.propName] = ops.value
+    }
+    Product.updateOne({
+        _id: id
+    },
+    {
+        $set: updatedOps
+    }
+    ).exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'Product updated',
+            request: {
+                type: 'GET',
+                url: `http://localhost:3000/products/${id}`
+            }
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
     })
 })
 
@@ -77,9 +138,22 @@ router.delete('/:productID', (req,res,next)=>{
     })
     .exec()
     .then(result =>{
-        res.status(200).json(result)
+        res.status(200).json({
+            message: 'Product deleted',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:3000/products',
+                body: {name: 'String', price: 'Number'}
+            }
+        })
     })
-    .catch()
+    .catch(err =>{
+        console.log(err);
+        res.status(500)
+        .json({
+            error: err
+        })
+    })
 })
 
 module.exports = router
--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const bodyParser = require('body-parser');
 const productsRouter = require('./api/routes/products')
 const ordersRouter = require('./api/routes/orders')
 
+const mongoose = require('mongoose');
+
+const escapedPassword = encodeURIComponent(process.env.MONGO_ATLAS_PW);
+mongoose.connect(`mongodb+srv://BhattBhavikR:${escapedPassword}@shop-products-orders.tdglcxy.mongodb.net/?retryWrites=true&w=majority`)
+
 app.use(morgan('dev'))
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -21,7 +26,7 @@ app.use((req,res,next)=>{
         "Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Origin, X-Requested-With"
         )
     if(req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods")
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
         return res;
     }
     next()
