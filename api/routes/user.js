1ecb793f224541f0840ee10d8d3992496caaa201
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-04-23 00:21:57 +0530

    added /user/login with jwt

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 
@@ -44,6 +44,51 @@ router.post('/signup', (req,res,next)=>{
                 }
             })
         }
+    })
+})
+
+router.post('/login', (req,res,next)=>{
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        if (user.length < 1){
+            return res.status(401).json({
+                 message: 'Auth failed'
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
+            if(err){
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            if(result){
+                const token = jwt.sign(
+                {
+                    email: user[0].email,
+                    userID: user[0]._id
+                }, 
+                process.env.JWT_KEY,
+                {
+                    expiresIn: "1h"
+                }
+                )
+                return res.status(200).json({
+                    message: 'Auth successful',
+                    token: token
+                })
+            }else{
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     })
 })
 
